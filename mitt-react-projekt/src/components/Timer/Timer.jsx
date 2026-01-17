49e8f693d7d1212803d85ccd9fbf5691881c8ed7
David Lindblom <lindblom.david@hotmail.com>
2026-01-17 21:19:01 +0100

    timer works now

--- mitt-react-projekt/src/components/Timer/Timer.jsx
+++ mitt-react-projekt/src/components/Timer/Timer.jsx
@@ -1,45 +1,58 @@
 import { useState, useEffect } from "react";
-
+import "./Timer.css"
 
 function Timer() {
-  const [currentTime, setCurrentTime] = useState(0);
-  const [timerToggle, setTimerToggle] = useState(false)
+  const [milliseconds, setMilliseconds] = useState(0);
+  // const [seconds, setSeconds] = useState(0);
+  // const [minutes, setMinutes] = useState(0);
+  // const [hours, setHours] = useState(0)
+  const [timerToggle, setTimerToggle] = useState(false);
 
-  // always run this? and then inside the TimerToggle will decide
-  const runTimer = () => {
-    setCurrentTime(currentTime => currentTime + 1);
-  }
-
-  const startTimer = () => {
-    
-  }
-
-  const stopTimer = () => {
-
+  const time = {
+    hours: Math.floor(milliseconds / 3600000),
+    minutes: Math.floor(milliseconds / 60000)%60,
+    seconds: Math.floor(milliseconds / 1000)%60, // 10*10
+    milliseconds: milliseconds
   }
 
   const resetTimer = () => {
-
+    setTimerToggle(false)
+    setMilliseconds(0)
   }
+
+  // TOGGLE TIMER
+  useEffect(() => {
+    console.log("runs")
+
+    if (timerToggle === true) {
+      //every "interval" gets an id when using SetInterval
+      const intervalId = setInterval(() => {
+        setMilliseconds(prev => prev + 100);
+      }, 100) // updating every 1ms made it slow, so 100ms instead
+
+      // removes the previous interval, so only 1 interval runs
+      return () => {
+        console.log("return")
+        clearInterval(intervalId)
+      }
+    } 
+
+    console.log("efter if")
+
+  }, [timerToggle])
 
   // insert use effect here for timerToggle
 
   return (
     <>
-      <div className="time-display">{currentTime}</div>
+      <div className="time-display">{`${time.hours}h : ${time.minutes}min : ${time.seconds}s`}</div>
       <div className="buttons">
-        <button onClick={runTimer}>Start</button>
-        <button onClick={stopTimer}>Stop</button>
+        <button onClick={() => setTimerToggle(true)}>Start</button>
+        <button onClick={() => setTimerToggle(false)}>Stop</button>
         <button onClick={resetTimer}>Reset</button>
       </div>
     </>
   )
 }
-
-
-
-
-
-
 
 export default Timer;
