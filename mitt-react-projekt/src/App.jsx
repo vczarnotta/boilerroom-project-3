bab7e81d80f331a9d750113d4d7c78f666c59efa
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-01-20 15:05:05 +0100

    fixat spacing

--- mitt-react-projekt/src/App.jsx
+++ mitt-react-projekt/src/App.jsx
@@ -2,9 +2,9 @@ import './App.css'
 import Dashboard from './pages/Dashboard'
 
 function App() {
-    return(
-        <Dashboard />
-    )
+  return(
+    <Dashboard />
+  )
 }
 
 export default App
