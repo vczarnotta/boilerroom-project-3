456d3cc03fb1b827168ccbb04a774d072a144e84
veronica.czarnotta@chasacademy.se <veronica.czarnotta@chasacademy.se>
2026-01-23 15:53:17 +0100

    gjort nav (#141) och home (#95)

--- mitt-react-projekt/src/pages/Dashboard.jsx
+++ mitt-react-projekt/src/pages/Dashboard.jsx
@@ -2,22 +2,11 @@ import Header from "../components/Header/Header"
 import Card from "../components/Cards/Cards"
 
 function Dashboard() {
-    return(
-        <>
-        <Header />
-        <main>
-            <Card 
-                // Lägg koppling till antalet tasks här
-                title={"0/0"}
-                description={"Uppgifter"}
-            />
-            <Card 
-                title={"Titel"}
-                description={"Beskrivning"}
-            />
-        </main>
-        </>
-    )
+  return(
+    <div className="main-container">
+
+    </div>
+  )
 }
 
 export default Dashboard
---
+++ mitt-react-projekt/src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import Header from "../components/Header/Header"
+import GridContainer from "../components/GridContainer/GridContainer"
+import Card from "../components/Cards/Cards"
+
+function Home() {
+  return(
+    <div className="main-container">
+      <GridContainer columns={4}>
+        <Card
+          // Här hämtas tiden från historik
+          title={"1h"}
+          children={<p>Fokustid idag</p>}
+        />
+
+        <Card
+          // Här hämtas tiden från historik
+          title={"12h"}
+          children={<p>Fokustid senaste veckan</p>}
+        />
+
+        <Card
+          //Här dyker ett peppande citat upp
+          title={<p>PEPP!!!</p>}
+          span={2}
+        />
+      </GridContainer>
+
+      <GridContainer columns={2} fullheight={true}>
+        <Card
+          title={<p>Starta timer</p>}
+        />
+
+        <Card
+          title={<p>Uppgifter</p>}
+          children={<p>Här listas uppgifter från ToDos</p>}
+        />
+      </GridContainer>
+    </div>
+  )
+}
+
+export default Home
