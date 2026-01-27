a58e29ab86f840d3930f5bb43412b8e45e22c0d7
nm <nm.codes@hotmail.com>
2026-01-27 01:08:06 +0100

    -

--- mitt-react-projekt/src/components/WorkSession/WorkSession.jsx
+++ mitt-react-projekt/src/components/WorkSession/WorkSession.jsx
@@ -1,7 +1,7 @@
 import {useState} from 'react';
 import {WorkSessionForm} from './WorkSessionForm';
 import {SessionCard} from './SessionCard';
-import styles from './WorkSession.css';
+import './WorkSession.css';
 
 export default function WorkSession(){
     const [sessions, setSessions] = useState([]);
